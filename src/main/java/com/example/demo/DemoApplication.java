8e864a50df1af5dafdf60d12129789e23658c7a8
Garba79 <garbarinogabriel@gmail.com>
2022-05-16 16:07:19 -0300

    Update DemoApplication.java

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -12,7 +12,7 @@ public class DemoApplication {
 
     @RequestMapping("/")
     String home() {
-        return "Hello World Gabi 15:24!";
+        return "Hello World Gabi 16:07!";
     }
 
     public static void main(String[] args) {
